d711e8373783f2bd7f23edda847d9636f3d84790
Nick Banks <nibanks@microsoft.com>
2022-02-11 21:51:56 +0000

    More code

--- lib/msh3.cpp
+++ lib/msh3.cpp
@@ -9,13 +9,35 @@
 #include "msh3.h"
 
 extern "C"
+MSH3_HANDLE
+MSH3_API
+MsH3Open(
+    void
+    )
+{
+    return nullptr;
+}
+
+extern "C"
+void
+MSH3_API
+MsH3Close(
+    MSH3_HANDLE* MsH3
+    )
+{
+    MsH3;
+}
+
+extern "C"
 void
 MSH3_API
 MsH3Get(
+    MSH3_HANDLE* MsH3,
     const char* ServerName,
     const char* Path
     )
 {
+    MsH3;
     ServerName;
     Path;
 }
