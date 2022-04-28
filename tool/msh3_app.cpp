828b9b3613e07a09798923944042dd65d40781da
Nick Banks <nibanks@microsoft.com>
2022-04-28 14:58:09 +0000

    Refactor app

--- tool/msh3_app.cpp
+++ tool/msh3_app.cpp
@@ -9,11 +9,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <vector>
 
-bool Print = true;
+using namespace std;
+
+struct Arguments {
+    const char* Host { nullptr };
+    uint16_t Port { 443 };
+    vector<const char*> Paths;
+    bool Unsecure { false };
+    bool Print { false };
+    uint32_t Count { 1 };
+    Arguments() { }
+} Args;
 
 void MSH3_CALL HeaderReceived(MSH3_REQUEST* , void* , const MSH3_HEADER* Header) {
-    if (Print) {
+    if (Args.Print) {
         fwrite(Header->Name, 1, Header->NameLength, stdout);
         printf(":");
         fwrite(Header->Value, 1, Header->ValueLength, stdout);
@@ -22,12 +33,12 @@ void MSH3_CALL HeaderReceived(MSH3_REQUEST* , void* , const MSH3_HEADER* Header)
 }
 
 void MSH3_CALL DataReceived(MSH3_REQUEST* , void* , uint32_t Length, const uint8_t* Data) {
-    if (Print) fwrite(Data, 1, Length, stdout);
+    if (Args.Print) fwrite(Data, 1, Length, stdout);
 }
 
 void MSH3_CALL Complete(MSH3_REQUEST* , void* Context, bool Aborted, uint64_t AbortError) {
     const uint32_t Index = (uint32_t)(size_t)Context;
-    if (Print) printf("\n");
+    if (Args.Print) printf("\n");
     if (Aborted) printf("Request %u aborted: 0x%llx\n", Index, (long long unsigned)AbortError);
     else         printf("Request %u complete\n", Index);
 }
@@ -38,75 +49,96 @@ void MSH3_CALL Shutdown(MSH3_REQUEST* Request, void* ) {
 
 const MSH3_REQUEST_IF Callbacks = { HeaderReceived, DataReceived, Complete, Shutdown };
 
-int MSH3_CALL main(int argc, char **argv) {
-    if (argc > 1 && (!strcmp(argv[1], "?") || !strcmp(argv[1], "help"))) {
-        printf("Usage: msh3 [server] [path] [unsecure] [count]\n");
-        return 1;
+void ParseArgs(int argc, char **argv) {
+    if (argc < 2 || !strcmp(argv[1], "-?") || !strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
+        printf("usage: %s <server[:port]> [options...]\n"
+               " -c, --count <num>      The number of times to query each path (def=1)\n"
+               " -h, --help             Prints this help text\n"
+               " -p, --path <path(s)>   The paths to query\n"
+               " -u, --unsecure         Allows unsecure connections\n"
+               " -v, --verbose          Enables verbose output\n"
+               " -V, --version          Prints out the version\n",
+              argv[0]);
+        exit(-1);
     }
 
-    const char* Host = "outlook.office.com";
-    int Port = 443;
-    char *givenHost = NULL;
-    const char* Path = "/";
-    bool Unsecure = false;
-    uint32_t Count = 1;
+    // Parse the server[:port] argument.
+    Args.Host = argv[1];
+    char *port = strrchr(argv[1], ':');
+    if (port) {
+        *port = 0; port++;
+        Args.Port = (uint16_t)atoi(port);
+    }
 
-    if (argc > 1) {
-        if(strrchr(argv[1], ':') != NULL) {
-            const int len = (int)strlen(argv[1]);
-            givenHost = (char *)calloc(len + sizeof(char), sizeof(char));
-            if (givenHost == NULL) {
-                printf("Failed to allocate memory\n");
-                return -1;
-            }
-#ifdef _WIN32
-            int cnt = sscanf_s(argv[1], "%[^:]:%d", givenHost, len, &Port);
-#else
-            int cnt = sscanf(argv[1], "%[^:]:%d", givenHost, &Port);
-#endif
-            if (cnt != 2) {
-                printf("Failed to parse server address\n");
-                return -1;
-            }
-            Host = givenHost;
-        } else {
-            Host = argv[1];
+    // Parse options.
+    for (int i = 2; i < argc; ++i) {
+        if (!strcmp(argv[i], "--count") || !strcmp(argv[i], "-c")) {
+            if (++i >= argc) { printf("Missing count value\n"); exit(-1); }
+            Args.Count = (uint32_t)atoi(argv[i]);
+
+        } else if (!strcmp(argv[i], "--path") || !strcmp(argv[i], "-p")) {
+            if (++i >= argc) { printf("Missing path value(s)\n"); exit(-1); }
+
+            char* Path = (char*)argv[1];
+            do {
+                char* End = strchr(Path, ',');
+                if (End) *End = 0;
+                Args.Paths.push_back(Path);
+                if (!End) break;
+                Path = End + 1;
+            } while (true);
+
+        } else if (!strcmp(argv[i], "--unsecure") || !strcmp(argv[i], "-u")) {
+            Args.Unsecure = true;
+
+        } else if (!strcmp(argv[i], "--verbose") || !strcmp(argv[i], "-v")) {
+            Args.Print = true;
+
+        } else if (!strcmp(argv[i], "--version") || !strcmp(argv[i], "-V")) {
+            uint32_t Version[4];
+            MsH3Version(Version);
+            printf("Using msh3 v%u.%u.%u.%u\n", Version[0], Version[1], Version[2], Version[3]);
         }
     }
-    if (argc > 2) Path = argv[2];
-    if (argc > 3 && !strcmp(argv[3], "unsecure")) Unsecure = true;
-    if (argc > 4) Count = (uint32_t)atoi(argv[4]);
-    Print = Count == 1;
 
-    const MSH3_HEADER Headers[] = {
+    if (Args.Paths.empty()) {
+        Args.Paths.push_back("/");
+    }
+}
+
+int MSH3_CALL main(int argc, char **argv) {
+    ParseArgs(argc, argv);
+
+    MSH3_HEADER Headers[] = {
         { ":method", 7, "GET", 3 },
-        { ":path", 5, Path, strlen(Path) },
+        { ":path", 5, Args.Paths[0], strlen(Args.Paths[0]) },
         { ":scheme", 7, "https", 5 },
-        { ":authority", 10, Host, strlen(Host) },
+        { ":authority", 10, Args.Host, strlen(Args.Host) },
         { "user-agent", 10, "curl/7.82.0-DEV", 15 },
         { "accept", 6, "*/*", 3 },
     };
     const size_t HeadersCount = sizeof(Headers)/sizeof(MSH3_HEADER);
 
-    printf("HTTP/3 GET https://%s:%d%s\n\n", Host, Port, Path);
-
     auto Api = MsH3ApiOpen();
     if (Api) {
-        auto Connection = MsH3ConnectionOpen(Api, Host, (uint16_t)Port, Unsecure);
+        auto Connection = MsH3ConnectionOpen(Api, Args.Host, Args.Port, Args.Unsecure);
         if (Connection) {
-            for (uint32_t i = 0; i < Count; ++i) {
-                auto Request = MsH3RequestOpen(Connection, &Callbacks, (void*)(size_t)(i+1), Headers, HeadersCount);
-                if (!Request) {
-                    printf("Request %u failed to start\n", i+1);
-                    break;
+            for (auto Path : Args.Paths) {
+                printf("HTTP/3 GET https://%s:%d%s\n\n", Args.Host, Args.Port, Path);
+                Headers[1].Value = Path;
+                Headers[1].ValueLength = strlen(Path);
+                for (uint32_t i = 0; i < Args.Count; ++i) {
+                    auto Request = MsH3RequestOpen(Connection, &Callbacks, (void*)(size_t)(i+1), Headers, HeadersCount);
+                    if (!Request) {
+                        printf("Request %u failed to start\n", i+1);
+                        break;
+                    }
                 }
             }
             MsH3ConnectionClose(Connection);
         }
         MsH3ApiClose(Api);
     }
-
-    free(givenHost);
 
     return 0;
 }
